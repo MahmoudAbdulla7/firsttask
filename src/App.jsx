34891bed79f10b731f1ad686f16679fcaeaa1560
MahmoudAbdalla7 <mahmoudapdallah629@gmail.com>
2023-10-21 18:40:46 +0300

    fixed  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Contacts from './components/Contacts/Contacts';
 import AddUser from './components/AddUser/AddUser';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import NotFound from './components/NotFound/NotFound';
 const router =createBrowserRouter([{
   path:"/firsttask",element:<Layout/>,children:[
     {index:true,element:<Contacts/>},
     {path:"adduser",element:<AddUser/>},
+    {path:"*",element:<NotFound/>},
   ]
 }])
 
